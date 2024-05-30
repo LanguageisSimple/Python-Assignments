fdede4103cc430ef590bd08631884da0901cc256
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-05-30 14:40:34 +0530

    Delete convert.py

--- convert.py
+++
@@ -1,8 +0,0 @@
-#Taking input from user in floating point datatype
-a = float(input("Enter a number : "))
-
-#Storing the integer value of a in another variable name 'b'
-b = int(a)
-
-#Printing the value of b which is the integer value of a
-print(b)
