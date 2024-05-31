a57bd4e59583b298a76a5852e89f1e14d9b0c8f6
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-05-31 23:54:23 +0530

    Uploading Assignment 4.py file
    Adding the Assignment 4.py file to the folder which is the solution to the assignment.

---
+++ Assignment 4/Assignment 4.py
@@ -0,0 +1,11 @@
+#Taking input from user and storing it in in a variable named as 'a'
+a = int(input("Enter the first number : "))
+
+#Taking the second input from user and storing it in a variable named as 'b'
+b = int(input("Enter the second number :"))
+
+#usig the operator '**='
+a**=b
+
+#Printing the result
+print(a)
