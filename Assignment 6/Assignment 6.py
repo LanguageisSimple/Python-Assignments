b6ec81b03e7f9e4adfc9bcc5c3e5f51e4e06efbc
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-02 13:49:26 +0530

    Uploading the answer to the assignment
    Adding the answer i.e., Assignment 6.py file which is the solution to the assignment.

---
+++ Assignment 6/Assignment 6.py
@@ -0,0 +1,11 @@
+#Importing the math package in the program
+import math
+
+#Taking user's input
+num = int(input("Enter a number : "))
+
+#Finding the squareroot of the user's entered number
+sqr = math.sqrt(num)
+
+#Printing the result
+print(f"The square root of {num} is : {sqr}")
