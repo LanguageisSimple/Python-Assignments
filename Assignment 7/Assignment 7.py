5262a8ec7974be1da9dd8639b03b05c8bcdeeef1
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-02 13:59:50 +0530

    Uploading answer to the assignment.
    Adding the answer to the assignment in the folder.

---
+++ Assignment 7/Assignment 7.py
@@ -0,0 +1,11 @@
+#Taking input from user and storing it in in a variable named as 'a'
+a = int(input("Enter the first number : "))
+
+#Taking the second input from user and storing it in a variable named as 'b'
+b = int(input("Enter the second number :"))
+
+#Substracting b from a and storing it in a 
+a-=b
+
+#Printing the result
+print(a)
