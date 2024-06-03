b511eaedfed30d49618cbe067061d8e152260f27
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-03 22:49:37 +0530

    Uploading Assignment 8.py
    Adding the answer to the assignment in the folder.

---
+++ Assignment 8/Assignment 8.py
@@ -0,0 +1,11 @@
+#Taking input from user and storing it in in a variable named as 'a'
+a = int(input("Enter the first number : "))
+
+#Taking the second input from user and storing it in a variable named as 'b'
+b = int(input("Enter the second number :"))
+
+#Using the operator '*=' on a and b
+a*=b
+
+#printting the result
+print(a)
