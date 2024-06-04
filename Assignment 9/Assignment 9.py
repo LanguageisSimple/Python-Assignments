76d0db39dcb50cbc670f34a102e7781e3f18f340
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-04 23:59:15 +0530

    Uploading Assignment 9.py
    Adding the answer to the assignment in the directory.

---
+++ Assignment 9/Assignment 9.py
@@ -0,0 +1,11 @@
+#Taking input from user and storing it in in a variable named as 'a'
+a = int(input("Enter the 1st number : "))
+
+#Taking the second input from user and storing it in a variable named as 'b'
+b = int(input("Enter the 2nd number : ")) 
+
+#uisng the operator '+=' on a and b
+a+=b
+
+#Printing the result
+print(a)
