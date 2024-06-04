8d8a353646a158fc375d755d79db6951f45faafd
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-05 02:29:29 +0530

    Uploading Assignment 10.py
    Adding the answer to the assignment in the directory.

---
+++ Assignment 10/Assignment 10.py
@@ -0,0 +1,11 @@
+#Taking input from user and storing it in in a variable named as 'a'
+a = int(input("Enter the first number : "))
+
+#Taking the second input from user and storing it in a variable named as 'b'
+b = int(input("Enter the second number :"))
+
+#Using the operator '%=' on a and b
+a%=b
+
+#Printing the result
+print(a)
